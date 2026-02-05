a3f4ab69a21a75d8d2548e1ea3ae263adf2e8f39
mathew <galbraithmath@gmail.com>
2026-02-05 18:47:38 +0000

    Adding in Envi Web_API_PORT logic w/error

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
+from flask import Flask
+
+
+app = Flask(__name__)
 
 WEB_API_PORT = os.getenv('WEB_API_PORT')
 WEB_API_VALUE = os.getenv('WEB_API_VALUE')
-
-
-from flask import Flask
-
-app = Flask(__name__)
 
 @app.route("/health")
 def health():
@@ -14,7 +13,14 @@ def health():
 
 
 
+"""
+ if __name__ == '__main__' Allows code to only run when script is executed.
+ Added some validation incase if 'WEB_API_PORT' is not set as an Enviornment variable
 
+"""
 
 if __name__ == '__main__':
-    app.run(debug=True, )
+    if os.getenv('WEB_API_PORT') == None :
+        print("Please assign a port to the enviornment variable WEB_API_PORT")
+    else:
+        app.run(debug=True, port=WEB_API_PORT)
