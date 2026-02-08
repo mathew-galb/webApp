685fc7be332a79d121fe6e0c8a71712500248f8e
mathew <galbraithmath@gmail.com>
2026-02-08 21:13:19 +0000

    Adding Comments, Fixing returns

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask
 
+"""
+Flask Documentation: 
+https://flask.palletsprojects.com/en/stable/
 
+OS lib: 
+https://docs.python.org/3/library/os.html
+"""
 
 app = Flask(__name__)
 
-WEB_API_PORT = os.getenv('WEB_API_PORT')
-WEB_API_VALUE = os.getenv('WEB_API_VALUE')
+# WEB_API_PORT = os.getenv('WEB_API_PORT')
+# WEB_API_VALUE = os.getenv('WEB_API_VALUE')
 
 @app.route("/health")
 def health():
@@ -14,13 +20,19 @@ def health():
 
 @app.route("/value")
 def value():
-    return WEB_API_VALUE
+    return os.getenv('WEB_API_VALUE')
 
 
 
 """
- if __name__ == '__main__' Allows code to only run when script is executed.
+ if __name__ == '__main__' Allows app.run to run when script is executed.
  Added some validation incase if 'WEB_API_PORT' is not set as an Enviornment variable
+
+ Took a while to find port as an optional parameter in the documentation but found this:
+ https://www.geeksforgeeks.org/python/how-to-change-port-in-flask-app/
+
+ On docker, I couldnt get the app to work, found that there was option to set host for external visbility 
+ and it helped; https://flask.palletsprojects.com/en/stable/quickstart/#a-minimal-application
 
 """
 
@@ -30,6 +42,6 @@ if __name__ == '__main__':
     if os.getenv('WEB_API_VALUE') == None:
         print("Please assign a value to the enviornment variable WEB_API_VALUE")
     else:
-        app.run(host="0.0.0.0", port=WEB_API_PORT)
-        #Adding host="0.0.0.0" fixed my none response problem
+        app.run(host="0.0.0.0", port=os.getenv('WEB_API_PORT'))
+        #Adding host="0.0.0.0" fixed my none response problem (only for docker)
 
--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,22 @@ import os
 import pytest
 from main import app
 
+"""
+Used these documentation as a starter.
+https://code.visualstudio.com/docs/python/testing
+https://docs.pytest.org/en/stable/getting-started.html
+
+Found that monkeypatch was a way to simulate environement variables
+https://docs.pytest.org/en/stable/how-to/monkeypatch.html
+
+took a while to find a way to get env through monkey patch but found that documentation
+makes use of os lib again.
+
+"""
+
 @pytest.fixture
 def test1(monkeypatch):
-    monkeypatch.setenv("WEB_API_PORT", "5000")
+    monkeypatch.setenv("WEB_API_PORT", "3000")
     monkeypatch.setenv("WEB_API_VALUE", "test_value")
     
     # Enable Flask testing mode
@@ -17,12 +30,20 @@ def test_health(test1):
     response = test1.get("/health")
     assert response.status_code == 200
     assert response.data == b"OK"
-    assert os.getenv("WEB_API_PORT") == "5000"
+    assert os.getenv("WEB_API_PORT") == "3000"
+    
 
-
+"""
+This test help me find out the way I was returning value was wrong
+"""
 def test_value(test1):
     """Test /health endpoint"""
     response = test1.get("/value")
     assert response.status_code == 200
+    assert response.data == b"test_value"
     assert os.getenv("WEB_API_VALUE") == "test_value"
-    assert os.getenv("WEB_API_PORT") == "5000"
+    assert os.getenv("WEB_API_PORT") == "3000"
+    
+
+#################################################
+
